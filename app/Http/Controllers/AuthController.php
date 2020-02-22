0811519076d6f7a63320ac22600cacecbd9afce9
alchristophergwapo <jankenneth.diansin@student.passerellesnumeriques.org>
2020-02-22 10:43:22 +0800

    added authenticatiion controller

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Validator,Redirect,Response;
+Use App\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Session;
+
+class AuthController extends Controller
+{
+    public function Login(Request $request) {
+        request()->validate([
+            'username'=> 'required',
+            'password'=>'required'
+        ]);
+
+        $credentials = $request->only('username', 'password');
+        if (Auth::attempt($credentials)) {
+            return redirect()->intended('dashboard');
+        }
+        return Redirect::to("login")->withError('error','Oooops! You have entered invalid credentials');
+    }
+
+    public function Registration(Request $request)
+    {  
+        request()->validate([
+        'username' => 'required',
+        'email' => 'required|email|unique:users',
+        'password' => 'required|min:6',
+        ]);
+         
+        $data = $request->all();
+ 
+        $check = $this->create($data);
+       
+        return Redirect::to("dashboard")->withSuccess('Great! You have Successfully loggedin');
+    }
+     
+    public function dashboard()
+    {
+ 
+      if(Auth::check()){
+        return view('dashboard');
+      }
+       return Redirect::to("login")->withSuccess('Opps! You do not have access');
+    }
+ 
+    public function create(array $data)
+    {
+      return User::create([
+        'username' => $data['name'],
+        'email' => $data['email'],
+        'password' => Hash::make($data['password'])
+      ]);
+    }
+     
+    public function logout() {
+        Session::flush();
+        Auth::logout();
+        return Redirect('login');
+    }
+}
